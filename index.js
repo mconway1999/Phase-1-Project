46ecebf7ab32fdf3e75bd1f92e615b44680572f8
Madeline Conway <conway.maddy@yahoo.com>
2024-09-04 13:28:08 -0400

    used fetch command for first object

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 alert('welcome to my project')
+fetch("http://localhost:3000/")
+    .then (response => response.json())
+    .then(burgers => {
+        console.log(burgers)
+    })
+    console.log("after the fetch...")
