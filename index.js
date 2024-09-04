db62a9f8915f75efa25e4abf61413f25c7431cbd
Madeline Conway <conway.maddy@yahoo.com>
2024-09-04 14:03:07 -0400

    burrito photo added

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 alert('welcome to my project')
 
+const restaurantMenuElement = document.getElementById('restaurant')
+
 function addBurgerImageToMenu(food){
- console.log(food)
+    const imgElement = document.createElement ('img')
+    imgElement.src = burger.image
+    restaurantMenuElement.appendChild(imgElement)
+
 }
 fetch("http://localhost:3000/")
     .then (response => response.json())
