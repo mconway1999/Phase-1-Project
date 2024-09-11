9d92fbbf465249555b1d2acca8cf80e7104e5e87
Madeline Conway <conway.maddy@yahoo.com>
2024-09-11 14:29:06 -0400

    completed

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 # Phase 1 Project 
 
 ## Introduction 
-This is a project where we can blah blah blah... 
+This is a project where users can create their own grocery lists virtually. 
 
-## Setup
+## Instructions 
 
-1. Run open 'index.html' file to open the 'index.html' in the browser.
+1. Run open `index.html` file to open the `index.html` in the browser.
 
+2. Open a new terminal `open index.html` in the terminal to open `index.html` file in the browser. 
+
+3. Put your mouse over images to see all the image details. This should display a name, category, and price. 
+
+4. Add new grocery items by filling out the form on the bottom of the page. Be sure to refresh the page after after comepleting the form.
+
+5. Delete grocery items by hitting the 'Delete Item' button. 
+
+## Conclusion 
+This application allows users to keep track of specific details in a list such as image, name, price, and category. It also allows for users to add new grocerys to a list by filling out a form. Lastly, this application allows for users to remove grocery items once purchased.  
--- index.js
+++ index.js
@@ -1,18 +1,96 @@
-alert('welcome to my project')
+const groceryList = document.querySelector('#grocery-list')
 
-const restaurantMenuElement = document.getElementById('restaurant')
+   const addForm = document.getElementById('add-grocery-items')
+    addForm.addEventListener('submit', function (event) {
+        event.preventDefault()
+        console.log(event)
 
-function addBurgerImageToMenu(food){
-    const imgElement = document.createElement ('img')
-    imgElement.src = burger.image
-    restaurantMenuElement.appendChild(imgElement)
+    const addNewName = document.getElementById("new-name")
+    const addNewCategory = document.getElementById("new-category")
+    const addNewPrice = document.getElementById('new-price')
+    const addNewImage = document.getElementById('new-image')
 
+    const newGrocery = {
+        name: addNewName.value,
+        category:addNewCategory.value,
+        price:addNewPrice.value,
+        img:addNewImage.value
+    }
+
+    addNewGrocery(newGrocery)
+    }) 
+
+
+function addGroceryImagesToBrowser(item){
+    const groceryDiv = document.createElement('div')
+    const buttonElement = document.createElement('button')
+    const imgElement = document.createElement('img')
+    imgElement.src = item.img
+    groceryList.appendChild(groceryDiv)
+    groceryDiv.appendChild(imgElement)
+    groceryDiv.appendChild(buttonElement)
+    buttonElement.textContent = "Delete Item"
+    
+   imgElement.addEventListener("mouseover", (event) => {
+      const nameH3 = document.createElement('h3')
+      nameH3.textContent = item.name
+      const priceH3 = document.createElement('h3')
+      priceH3.textContent =item.price
+      const categoryH3 = document.createElement('h3')
+      categoryH3.textContent = item.category
+      
+      groceryDiv.append(nameH3,priceH3,categoryH3)
+    
+    });
+    buttonElement.addEventListener('click',()=>{
+        groceryDiv.remove()
+        console.log('hello')
+        })
+          
+    
+        imgElement.addEventListener('mouseleave',(event)=>{
+            groceryDiv.innerHTML = "";
+            groceryDiv.append(imgElement)
+            groceryDiv.append(buttonElement)
+        })
 }
-fetch("http://localhost:3000/")
-    .then (response => response.json())
-    .then(food => {
-        console.log(food)
-        food.forEach(addBurgerImageToMenu)
+
+function displayGroceries(groceries){
+    const groceryImage = document.querySelector ('.detail-image')
+    groceryImage.src = food.image
+}
+
+
+fetch("http://localhost:3000/groceries")
+  .then((resp) => resp.json())
+  .then((json) => {
+    console.log(json)
+    json.forEach((item)=>{
+        addGroceryImagesToBrowser(item)
     })
+
+  });
+
+  function addNewGrocery (newGrocery){
+    fetch('http://localhost:3000/groceries',{
+        method:'POST',
+        headers:{
+            "Content-type" : "application/json"
+        },
+        body:JSON.stringify(newGrocery)
+    })
+    .then (response => response.json())
+    .then (groceries => console.log(groceries))
+}
+ 
+
+
+
+
+
+
    
+
+
+
     
